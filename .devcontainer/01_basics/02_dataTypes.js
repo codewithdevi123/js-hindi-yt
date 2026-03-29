894108bb8caa619fcea1ea8e226a5909776f6e60
codewithdevi123 <kantadevi998355@gmail.com>
2026-03-29 08:38:35 +0000

    data

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js  code as newer version
+
+// alert(3++3) // we ar using node js, not browser
+
+console.log(3+3)
+ console.log("Ashish")
+// code readablity should be high
+
+let name = "Ashish"
+
+let age = 23
+
+let isloggedIn = false
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standanole value
+// undefined =>
+// symbol => unique
+// object
+
+console.log(typeof object);
+
+
