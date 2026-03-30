72ce370dd4010045aa69374155b2e3bd2b857ea0
codewithdevi123 <kantadevi998355@gmail.com>
2026-03-30 17:12:35 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "Ashish"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+//  "33" => 33 
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Ashish"
+
+let booleanIsLogged = Boolean(isLoggedIn)
+// console.log(booleanIsLogged);
+
+//  when you write 1  => true; 0 => false;
+// "" it means (Mt string) => false;
+// "Ashish" => true
+
+let  someNumber = 33
+let stringNumbr =  String(someNumber)
+
+console.log(stringNumbr);
+
+console.log(typeof stringNumbr);
+
