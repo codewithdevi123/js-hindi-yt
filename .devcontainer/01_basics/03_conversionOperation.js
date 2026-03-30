dd19c56ddcbbd5657c81e86ed0c781323c2bed7e
codewithdevi123 <kantadevi998355@gmail.com>
2026-03-30 18:07:55 +0000

    conversion of datatypes

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -24,7 +24,40 @@ let booleanIsLogged = Boolean(isLoggedIn)
 let  someNumber = 33
 let stringNumbr =  String(someNumber)
 
-console.log(stringNumbr);
+// console.log(stringNumbr);
 
-console.log(typeof stringNumbr);
+// console.log(typeof stringNumbr);
+
+
+// ******* Operations *******
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2**3);
+//console.log(2/4);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Ashish"
+let str3 = str1 + str2
+// console.log(str3);
+ 
+// console.log("1" + 2);
+// console.log(1 + "2");  
+
+// console.log("1" + 2 + 2);
+// console.log(1 + 1 + "2");
+//  console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+ num1 = num2 = num3 = 2 + 2
+let  gameCounter = 100
+++gameCounter
+
+console.log(gameCounter);
 
