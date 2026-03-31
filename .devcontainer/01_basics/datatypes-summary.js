712f1315822d726e4cd149788d69f94261ca7d7b
codewithdevi123 <kantadevi998355@gmail.com>
2026-03-31 18:02:04 +0000

    datatype summary

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,28 @@
+// 1.) Primitive
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// 2.) Reference (Non Primitiv)
+// Array, Objects, Functions        
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+//console.log(id === anotherId);
+
+//const bigNumber =773700081256123232111n
+const heros = ["krish", "balveer", "bhim"]
+let myObj = {
+    name: "Ashish",
+age: 22
+}
+
+const myFunction = function (){
+    console.log("hello world");
+}
+//console.log(typeof bigNumber);
+// console.log(typeof outsideTemp);
+//datatypes table link => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Data_structures
