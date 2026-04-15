5fe8d2b6863b89db52421aca497c4e79631cf952
codewithdevi123 <kantadevi998355@gmail.com>
2026-04-15 18:04:41 +0000

    datesinin js

---
+++ .devcontainer/01_basics/07_datesinjs.js
@@ -0,0 +1,23 @@
+// dates
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(typeof myDate);
+
+
+// let myCreatedDate = new Date(2023, 0, 23, 5, 33) //months "0" se start hote he java script me
+// console.log(myCreatedDate.toLocaleString());
+
+    // let myCreatedDate = new Date("2023-01-14")
+//let myCreatedDate = new Date("2023-00-14")
+
+let myCreatedDate = new Date("01-14-2023")
+
+// //console.log(myCreatedDate.toLocaleString());
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+let newDate = newDate()
+console.log(newDate);
