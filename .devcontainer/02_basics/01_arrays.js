7591598bedc3840dabe92829f3e6d01cedabc10d
codewithdevi123 <kantadevi998355@gmail.com>
2026-05-14 18:15:46 +0000

    arrays

---
+++ .devcontainer/02_basics/01_arrays.js
@@ -0,0 +1,29 @@
+// array
+
+const myArr = [0,1,2,3,4,5]
+
+const myHeors = ["shaktiman", "naagraj"]
+const myArr2 = new Array (1,2,3,4)
+
+// console.log(myArr[0])
+//Arrays methods
+// myArr.push(6) if u want an add more value in your main vaalue like 1.2.3.4.5+6 then u can use "push" 
+// myArr.pop() if u want remove ur last value in ur main value then u can use "pop" like if ur last added value is 6 and u want remove ur last value in ur main value then will use "pop" 
+// myArr. unshift(9)
+// console.log(myArr)
+// myArr. shift()
+// console.log(myArr)
+
+// console.log(myArr.includes(9));
+// const newArr = myArr.join()
+
+
+// console.log(myArr); 
+// slice , splice
+console.log("A", myArr);
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+console.log("B", myArr);
+const myn2 = myArr.splice(1,3)
+console.log("c",myArr);
+console.log(myn2);
